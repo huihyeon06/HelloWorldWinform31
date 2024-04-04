5a51477fefd260cf44bb8338537a4e63bcf52b4c
Huihyeon Park <agnea0829@gmail.com>
2024-04-04 11:03:30 +0900

    image, icon 삽입

--- HelloWorldWinform31/Form1.cs
+++ HelloWorldWinform31/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace HelloWorldWinform31
 {
-    public partial class Form1 : Form
+    public partial class Form1 : System.Windows.Forms.Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
